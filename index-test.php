9bda70a758a7fc1893e291fc7c56beb57d6ef9a2
Егор <egor.developer@gmail.com>
2012-06-25 17:03:28 +0300

    test

--- index-test.php
+++ index-test.php
@@ -1,4 +1,10 @@
 <?php
+//test
+echo '123';
+echo '123';
+echo '123';
+echo '123';echo '123';
+echo '123';
 /**
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
