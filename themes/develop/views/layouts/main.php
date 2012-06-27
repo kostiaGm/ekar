63ad39f5ebdbfb6e372fe2995657504692553cb9
kot <kot@localhost>
2012-06-27 08:47:59 +0300

    Добавление стилей для формы. Ветка kostia

--- themes/develop/views/layouts/main.php
+++ themes/develop/views/layouts/main.php
@@ -36,6 +36,7 @@
 	<?php endif?>
  <?php echo $content; ?>
 </div>
+    <div class="clear"></div>    
 <div id="footer">
 Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
