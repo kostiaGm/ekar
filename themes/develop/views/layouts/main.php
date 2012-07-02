cf229f52169f7f3ffb548eb12d93e814ba0b88b9
Егор <egor.developer@gmail.com>
2012-07-02 23:06:23 +0300

    Добавлена ссылка в меню

--- themes/develop/views/layouts/main.php
+++ themes/develop/views/layouts/main.php
@@ -25,6 +25,7 @@
     <?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+                                array('label'=>'test', 'url'=>array('/site/test/test2/test3')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
